a2d83897cfd2e195fe11e4deb10d9de142f4de8e
Khalid Ahmed mohamed <khalid.ahmed@oasisoft.net>
2024-12-31 00:41:06 +0200

    []

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../shared/components/ui/shared/shared.module';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, ProgressBarModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  value: Number = 70;
+}
